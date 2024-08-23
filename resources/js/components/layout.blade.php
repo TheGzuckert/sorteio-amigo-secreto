0bda81d395e62930ba8ad46d35e076d24b8e9e27
TheGzuckert <gustavo.santoszuckert@gmail.com>
2024-08-22 22:03:02 -0300

    fix: bootstrap styles and add mix

--- resources/js/components/layout.blade.php
+++ resources/js/components/layout.blade.php
@@ -1,15 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport", user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0>
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $title }}s</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title class="">{{$title}}</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/font/bootstrap-icons.min.css">
 </head>
 <body>
-  <h1>{{ $title }}</h1>
-
-  {{ $slot }}
+  <div class="container mt-5">
+    <h1 class="d-flex justfy-content-center aling-items-center">{{$title}}</h1>
+    {{$slot}}  
+  <div>
 </body>
 </html>
